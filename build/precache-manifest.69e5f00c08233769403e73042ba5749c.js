ec895d5332127e51958e5adbbd634c325ac58aa9
scorpidio11 <scorpido@gmail.com>
2018-12-19 20:53:08 -0700

    npm

---
+++ build/precache-manifest.69e5f00c08233769403e73042ba5749c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd07a9009a0d400f39c3",
+    "url": "/Big-Bang-Game/static/js/runtime~main.cd07a900.js"
+  },
+  {
+    "revision": "9f115eda43f549ded359",
+    "url": "/Big-Bang-Game/static/js/main.9f115eda.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/Big-Bang-Game/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "9f115eda43f549ded359",
+    "url": "/Big-Bang-Game/static/css/main.43946f92.chunk.css"
+  },
+  {
+    "revision": "c6c718883f5f89ed185f13da4452300f",
+    "url": "/Big-Bang-Game/index.html"
+  }
+];
